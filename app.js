da2f7efb3e4dd3ad89e3da2e336a2948ba119393
aaman2021 <amanstylox959@gmail.com>
2021-05-03 02:51:00 +0500

    JavaScript Assignment

---
+++ app.js
@@ -0,0 +1,113 @@
+// Chapter 1
+// Q 1
+// greeting = "Good morning";
+
+// Q2
+// var a="Error! Please Enter a valid Password";
+
+// alert(a);
+
+// Q3
+// var a="Welcome to js land \n Happy Coding";
+// alert(a);
+
+// Q4
+// alert("welcome to js land");
+// alert("Happy Coding\nprevent this page from creating additional dialoges");
+// Q5
+// console.log("hello i can run js code thought web,s browser console");
+// alert("hello i can run js code thought web,s browser console")
+// paste the above line to cansole and press enter
+
+
+
+// chapter 2
+// Q1
+// var username;
+// Q2
+// var MyName="Amman";
+// Q3
+// var message="Hello World";
+// alert(message)
+// Q4
+// studentname ="John Doe";
+// studentage = "10 Years Old";
+// studentcourse = "Certified Mobile applicatin developmnet";
+
+// alert(studentname);
+// alert(studentage);
+// alert(studentcourse);
+// Q5
+// var pizza="Pizza\npizz\npiz\npi\np";
+// alert(pizza);
+// Q6
+// var email="amanstylox959@gmail.com";
+// alert("My email adress is " + email);
+// Q7
+// var book="A smarter way to learn JavaScript";
+// alert ("i am trying to learn from the book "+book);
+// Q8
+// document.write("Yah! i can write Html content throught javascript");
+// Q9
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+// chapter 3
+// Q1
+// var age="20";
+// alert("i am " + age + " years old");
+
+// Q2
+// var birthyear="2000";
+// document.write(birthyear);
+// document.write("<br>");
+// document.write("data type of my decleared variable is number");
+
+// Q3
+// var Visitorsname="JohnDoe";
+// var Producttitle=  " on XYZ Clothing store";
+// var Quantity=" ordered 5 T-shirt(s)";
+// document.write(Visitorsname+Producttitle+Quantity);
+
+// chapter5
+
+// Q1
+// var num1=3;
+// var num2=5;
+// var result=num1+num2;
+// document.write("sum of " + num1 + " and "+num2+" is "+result);
+
+// Q2
+// var num1=5;
+// var num2=3;
+// var result=num1-num2;
+// document.write("Subtraction of " + num1 + " and "+num2+" is "+result);
+
+// var num1=3;
+// var num2=5;
+// var result=num1*num2;
+// document.write("Multiplication of " + num1 + " and "+num2+" is "+result);
+
+// var num1=3;
+// var num2=5;
+// var result=num1/num2;
+// document.write("Division of " + num1 + " and "+num2+" is "+result);
+
+//Q3
+// var equation;
+// document.write("Value after variable declaration is "+equation);
+// document.write("<br>");
+// equation=14;
+// document.write("Initial value "+equation);
+// document.write("<br>");
+// equation++;
+// document.write("Value after increment is "+equation);
+// document.write("<br>");
+// equation=equation+7;
+// document.write("Value after addition 7 numbers"+equation);
+// document.write("<br>");
+// equation--;
+// document.write("Value after decrement is "+equation);
+// document.write("<br>");
+// equation=equation%3;
+// document.write("The remainder is "+equation)
+
